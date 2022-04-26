75071e4bfa7beffc51b9129aa1461bc124e5e697
Gérald Lelong <gerald.lelong@easymov.fr>
2022-04-26 23:56:38 +0200

    actix-tls issue

--- flake.nix
+++ flake.nix
@@ -1,12 +1,25 @@
 {
-  inputs.dream2nix.url =
-    "github:nix-community/dream2nix/c93972bd52977de332812fcc97ff77f8b85bcf42";
-  outputs = { self, dream2nix }@inputs:
+  inputs = {
+    dream2nix.url =
+      "github:yusdacra/dream2nix/6d2a50459c51e4ffef5a3ce9165f9c682cd8fd22";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-21.05";
+  };
+  outputs = { self, dream2nix, nixpkgs }@inputs:
     let
-      pkgs = inputs.dream2nix.nixpkgs.legacyPackages.x86_64-linux;
       dream2nix = inputs.dream2nix.lib.init {
         systems = [ "x86_64-linux" ];
         config.projectRoot = ./.;
       };
-    in dream2nix.makeFlakeOutputs { source = ./.; };
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in dream2nix.makeFlakeOutputs {
+      source = ./.;
+      packageOverrides = {
+        ws_stream = {
+          add-pre-build-steps = {
+            nativeBuildInputs = old: old ++ (with pkgs; [ pkg-config ]);
+            buildInputs = old: old ++ (with pkgs; [ openssl ]);
+          };
+        };
+      };
+    };
 }
