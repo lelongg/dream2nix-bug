aa355bbc00ae96aa7074817cdeef58d2f95a0f36
Gérald Lelong <gerald.lelong@easymov.fr>
2022-04-26 13:02:21 +0200

    more crates

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   inputs.dream2nix.url =
-    "github:nix-community/dream2nix/72f35bb94e45928b8ce3426448dd8fb990d41034";
+    "github:nix-community/dream2nix/c93972bd52977de332812fcc97ff77f8b85bcf42";
   outputs = { self, dream2nix }@inputs:
     let
+      pkgs = inputs.dream2nix.nixpkgs.legacyPackages.x86_64-linux;
       dream2nix = inputs.dream2nix.lib.init {
         systems = [ "x86_64-linux" ];
         config.projectRoot = ./.;
